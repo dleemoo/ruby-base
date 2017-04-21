e3c543f6e8a7e9559582485df63807fc500cd5fe
Leonardo Lobo Lima <dleemoo@gmail.com>
2017-04-21 01:36:56 -0300

    [IMPROVEMENT] - Change build.sh to be more flexible

--- bin/build.sh
+++ bin/build.sh
@@ -1,43 +1,32 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 root_dir=$(pwd)
 
-cd base \
-   && docker build --rm -t base . \
-   && docker tag $(docker images -q base | head -n1) dleemoo/base:latest \
-   && cd $root_dir
+declare -a all_versions=(2.3.0 2.3.1 2.3.3 2.4.1)
 
-cd 2.3.0 \
-   && docker build --rm -t rb-230 . \
-   && docker tag $(docker images -q rb-230 | head -n1) dleemoo/ruby-base:2.3.0 \
-   && cd $root_dir
+if [ -z "$1" ]; then
+  declare -a list=(${all_versions[@]})
+else
+  declare -a list=()
+  for i in $@; do
+    [[ "${all_versions[@]}" =~ "$i" ]] && list=(${list[@]} $i)
+  done
+fi
 
-cd 2.3.1 \
-   && docker build --rm -t rb-231 . \
-   && docker tag $(docker images -q rb-231 | head -n1) dleemoo/ruby-base:2.3.1 \
-   && cd $root_dir
+function build() {
+  image=$1
+  repo=$2
 
-cd 2.3.3 \
-   && docker build --rm -t rb-233 . \
-   && docker tag $(docker images -q rb-231 | head -n1) dleemoo/ruby-base:2.3.3 \
-   && cd $root_dir
+  cd $image \
+     && docker build --rm -t $image . \
+     && docker tag $(docker images -q $image | head -n1) $repo \
+     && cd $root_dir
+}
 
-cd 2.4.1 \
-   && docker build --rm -t rb-241 . \
-   && docker tag $(docker images -q rb-241 | head -n1) dleemoo/ruby-base:2.4.1 \
-   && cd $root_dir
-
-cd app-2.3.0 \
-   && docker build --rm -t app-230 . \
-   && docker tag $(docker images -q app-230 | head -n1) dleemoo/app:2.3.0 \
-   && cd $root_dir
-
-cd app-2.3.3 \
-   && docker build --rm -t app-233 . \
-   && docker tag $(docker images -q app-231 | head -n1) dleemoo/app:2.3.3 \
-   && cd $root_dir
-
-cd app-2.4.1 \
-   && docker build --rm -t app-241 . \
-   && docker tag $(docker images -q app-241 | head -n1) dleemoo/app:2.4.1 \
-   && cd $root_dir
+if [ ${#list[@]} -gt 0 ]; then
+  build base "dleemoo/base:latest"
+  for i in "${list[@]}"; do
+    build "rb-${i}" "dleemoo/ruby-base:${i}"
+    build "app-${i}" "dleemoo/app:${i}"
+  done
+fi
