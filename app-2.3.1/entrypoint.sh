7156cde9c621227acda5fde367485305c1dfcaee
Leonardo Lobo Lima <dleemoo@gmail.com>
2018-05-29 22:11:30 -0300

    move gems PATH to /home/app/gems

--- app-2.3.1/entrypoint.sh
+++ app-2.3.1/entrypoint.sh
@@ -3,8 +3,9 @@
 USER_ID=${USER_ID:-1000}
 GROUP_ID=${GROUP_ID:-100}
 
+mkdir -p /home/app
 useradd --uid $USER_ID --gid $GROUP_ID -d /home/app app 2>/dev/null
-chown -R app. $APP_HOME $BUNDLE_PATH
+chown -R app. $APP_HOME $BUNDLE_PATH /home/app
 
 if [ -z "$*" ]; then
   exec gosu app bash
