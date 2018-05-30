d33246f17cbbcab45acbf5eee70ce503a08633b3
Leonardo Lobo Lima <dleemoo@gmail.com>
2018-05-29 21:11:38 -0300

    New ruby-base:0.0.4 image and build script improvements

--- bin/build.sh
+++ bin/build.sh
@@ -2,14 +2,19 @@
 
 root_dir=$(pwd)
 
-declare -a all_versions=(2.3.0 2.3.1 2.3.3 2.4.1 2.5.1)
+declare -a all_versions=(base 2.3.0 2.3.1 2.3.3 2.4.1 2.5.1)
 
 if [ -z "$1" ]; then
   declare -a list=(${all_versions[@]})
 else
   declare -a list=()
   for i in $@; do
-    [[ "${all_versions[@]}" =~ "$i" ]] && list=(${list[@]} $i)
+    if [[ " ${all_versions[@]} " =~ " $i " ]]; then
+      list=(${list[@]} $i)
+    else
+      echo "Invalid version: $i" >&2
+      exit
+    fi
   done
 fi
 
@@ -20,12 +25,15 @@ function build() {
   cd $dir \
      && docker build --rm -t $repo:latest . \
      && docker tag $repo:latest $repo:$(cat VERSION) \
+     && echo "$repo:$(cat VERSION) is the lastest" \
+     && echo -e "\n============\n" \
      && cd $root_dir
 }
 
-build base "dleemoo/base" &&
 if [ ${#list[@]} -gt 0 ]; then
+  build base "dleemoo/base" &&
   for i in "${list[@]}"; do
+    ([[ "$i" == "base" ]] && continue)
     build "rb-${i}" "dleemoo/ruby-base-${i}" &&
     build "app-${i}" "dleemoo/app-${i}"
   done
